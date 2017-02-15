4f9f5467a48058bf58a5494d70cc04e8586ecea5
Saivickna Raveendran <saivickna.r@gmail.com>
2017-02-15 12:31:06 -0800

    (feat) Add basic DB helpers and initialize server and db test spec files

--- src/db/db/helpers.js
+++ src/db/db/helpers.js
@@ -6,13 +6,15 @@ var q = Promise.promisify(db.query);
 module.exports = {
   user: {
     get: (id) => q('SELECT * FROM user where userID = $1', id),
-    post: (type, firstName, lastName, email, password, authID) => q('INSERT INTO ("userID","type","firstName","lastName","email","password","authID") VALUES ($1, $2, $3, $4, $5, $6, $7)', [type, firstName, lastName, email, password, authID])
+    post: (type, firstName, lastName, email, password, authID) => q('INSERT INTO user ("type","firstName","lastName","email","password","authID") VALUES ($1, $2, $3, $4, $5, $6)', [type, firstName, lastName, email, password, authID])
   },
   presentation: {
-
+    get: (id) => q('SELECT * FROM presentation where presentationID = $1', id),
+    post: (userID) => q('INSERT INTO presentation ("userID") VALUES ($1)', [userID])
   },
   questions: {
-
+    get: (id) => q('SELECT * FROM questions where questionsID = $1', id),
+    post: (presentationID, questionID) => q('INSERT INTO questions ("presentationID", "questionID") VALUES ($1, $2)', [presentationID, questionID])
   },
   question: {
     get: (id) => q('SELECT * FROM question WHERE questionID = $1', id),
