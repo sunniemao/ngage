782b3a9133d657ed9e17e8f41277572631df7172
pshen <patrick.lc.shen@gmail.com>
2017-02-15 12:08:02 -0800

    (feat) fill out helper get/post DB queries

--- src/db/db/helpers.js
+++ src/db/db/helpers.js
@@ -15,15 +15,21 @@ module.exports = {
 
   },
   question: {
-
+    get: (id) => q('SELECT * FROM question WHERE questionID = $1', id),
+    post: (type, question) => q('INSERT INTO question ("type", "question") VALUES ($1, $2)', [type, question])
   },
   answer: {
+    get: (id) => q('SELECT * FROM answer WHERE answerID = $1', id),
+    post: (questionID, answer, correct) => q('INSERT INTO answer ("questionID", "answer", "correct") VALUES ($1, $2, $3)', [questionID, answer, correct])
 
   },
   session: {
-    
+    get: (id) => q('SELECT * FROM session WHERE sessionID = $1', id),
+    post: (presentationID, socket) => q('INSERT INTO session ("presentationID", "socket") VALUES ($1, $2)', [presentationID, socket])
+
   },
   response: {
-    
+    get: (id) => q('SELECT * FROM response WHERE responseID = $1', id),
+    post: (sessionID, userID, questionID, answerID, content) => q('SELECT * FROM response ("sessionID", "userID", "questionID", "answerID", "content") VALUES ($1, $2, $3, $4, $5)', [sessionID, userID, questionID, answerID, content])
   }
 }
